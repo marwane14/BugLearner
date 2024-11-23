55128899fce09a8d67902991c6fc9c9ef466c8dc
Marwane14 <marwaneeljafari@gmail.com>
2024-11-23 22:06:40 +0100

    commencement

--- cve_scraper.py
+++ cve_scraper.py
@@ -1,8 +1,8 @@
-import requests
-
 def get_cve_data():
-    # Exemple de récupération des CVE depuis l'API du NVD
+    pass
+    '''
+    Exemple pas tres élaborer
     url = "url a mettre"
     response = requests.get(url)
     cve_data = response.json()
-    return cve_data['CVE_Items'][:10]  # Récupère les 10 dernières CVE par exemple
+    return cve_data['CVE_Items'][:10]'''  # Récupère les 10 dernières CVE par exemple
