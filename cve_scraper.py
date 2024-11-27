d9225e82f67d1e2c7a1ac8dd1c86685f5ea78473
Marwane <78431841+marwane14@users.noreply.github.com>
2024-11-27 14:51:00 +0100

    Delete cve_scraper.py

--- cve_scraper.py
+++
@@ -1,8 +0,0 @@
-def get_cve_data():
-    pass
-    '''
-    Exemple pas tres élaborer
-    url = "url a mettre"
-    response = requests.get(url)
-    cve_data = response.json()
-    return cve_data['CVE_Items'][:10]'''  # Récupère les 10 dernières CVE par exemple
